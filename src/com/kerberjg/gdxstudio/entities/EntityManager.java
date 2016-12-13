71326a11be4ecc84250caedc9cf63ae0494bdde1
Jakub Kerber <kerber.jg@gmail.com>
2016-12-13 23:47:33 +0100

    Partially implemented EntitySystem and documented EntityManager

--- src/com/kerberjg/gdxstudio/entities/EntityManager.java
+++ src/com/kerberjg/gdxstudio/entities/EntityManager.java
@@ -1,5 +1,7 @@
 package com.kerberjg.gdxstudio.entities;
 
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.kerberjg.gdxstudio.utils.collections.FastIntMap;
 import com.kerberjg.gdxstudio.utils.collections.HybridMap;
@@ -9,7 +11,7 @@ import com.kerberjg.gdxstudio.utils.collections.HybridMap;
  * Keeps Components in parallel arrays, sorted by their Entity ID
  * 
  * @author kerberjg */
-public class EntityManager {
+public class EntityManager implements Disposable {
 	/** A map of all entities that allows them to be reached both by their ID and their String name */
 	private HybridMap<String, Entity> entities = new HybridMap<String, Entity>();
 	/** A table holding all components, with Component types as rows and Entity IDs as columns */
@@ -22,45 +24,90 @@ public class EntityManager {
 		// TODO: do something useful here 
 	}
 	
+	@Override
+	/** Disposes of all entities, components and systems */
+	public void dispose() {
+		
+	}
+	
 	//TODO: document everything properly
 	
 	/*
 	 * Entity management
 	 */
 	
+	/** Adds an Entity to the manager
+	 * 
+	 * @param name the String name that the Entity can be accessed with
+	 * @param entity the Entity to be added
+	 * 
+	 * @return the ID of the new Entity */
 	protected int addEntity(String name, Entity entity) {
 		return entities.put(name, entity);
 	}
 	
+	/** @return whether an Entity with a specific name is present */
 	public boolean hasEntity(String name) {
 		return entities.containsKey(name);
 	}
 	
+	/** @return whether an Entity with a specific ID is present */
 	public boolean hasEntity(int id) {
 		return entities.containsId(id);
 	}
 	
+	/** @return the number of Entities currently present in the manager */
 	public int entityCount() {
 		return entities.size();
 	}
 	
+	/** @return an Entity referred to by its name */
 	public Entity getEntity(String name) {
 		return entities.get(name);
 	}
 	
+	/** @return an Entity referred to by its ID */
 	public Entity getEntity(int id) {
 		return entities.get(id);
 	}
 	
+	/** Finds all the Entity instances containing the requested Components
+	 * 
+	 * @param componentIds an array of Component IDs to find in Entities
+	 * @return an Iterable collection containing Entities that contain all the Components */
+	public Iterable<Entity> findEntities(int[] componentIds) {
+		Array<Entity> i = new Array<>(entities.size());
+		
+		for(Entity e : entities) {
+			int c = 0;
+			
+			// Counts the matching Components
+			for(int cid : componentIds)
+				if(components.get(cid).containsKey(e.id))
+					++c;
+			
+			// Add to iterable if all Components are found
+			if(c == componentIds.length)
+				i.add(e);
+		}
+		
+		return i;
+	}
+	
+	/** Removes an Entity, searching it by its name
+	 * @return the removed Entity */
 	public Entity removeEntity(String name) {
 		return entities.remove(name);
 	}
 	
+	/** Removes an Entity, searching it by its ID
+	 * @return the removed Entity */
 	public Entity removeEntity(int id) {
 		return entities.remove(id);
 	}
 	
 	public void clearEntities() {
+		// TODO: call dispose() on everything
 		entities.clear();
 		components.clear();
 	}
@@ -69,6 +116,7 @@ public class EntityManager {
 	 * Component management
 	 */
 	
+	/** Adds a Component to a specific Entity */
 	protected void addComponent(int entityId, Component component) {
 		if(entities.containsId(entityId)) {
 			getComponentMap(component.typeId).put(component);
@@ -76,10 +124,10 @@ public class EntityManager {
 			throw new RuntimeException("Entity with ID " + entityId + " is not present");
 	}
 	
+	/** @returns the Component of a specific type that belongs to a specific entity */
 	protected <C extends Component> C getComponent(int entityId, int componentType) {
 		@SuppressWarnings("unchecked")
 		Class<C> ct = (Class<C>) Components.getComponentClass(componentType);
-		
 		return ct.cast(getComponentMap(componentType).get(entityId));
 	}
 	
@@ -110,16 +158,30 @@ public class EntityManager {
 	 * System management
 	 */
 	
+	/** Adds an EntitySystem to the manager
+	 * @return an EntitySystem of the same type if previously present */
 	@SuppressWarnings("unchecked")
 	protected <S extends EntitySystem> S addSystem(S system) {
 		return (S) systems.put(system.getClass(), system);
 	}
 	
+	/** @return the EntitySystem identified by its class*/
 	protected <S extends EntitySystem> S getSystem(Class<S> systemType) {
 		return systemType.cast(systems.get(systemType));
 	}
 	
-	protected <S extends EntitySystem> S removeSystem(Class<S> systemType) {
-		return systemType.cast(systems.remove(systemType));
+	/** Removes an EntitySystem from the manager, calling its cleanup() and dispose() methods
+	 * 
+	 * @return whether the system was actually present and removed */
+	protected <S extends EntitySystem> boolean removeSystem(Class<S> systemType) {
+		EntitySystem es = systems.remove(systemType);
+		
+		if(es != null) {
+			es.cleanup();
+			es.dispose();
+			return true;
+		} else
+			return false;
 	}
+	
 }
--- src/com/kerberjg/gdxstudio/entities/EntitySystem.java
+++ src/com/kerberjg/gdxstudio/entities/EntitySystem.java
@@ -1,5 +1,8 @@
 package com.kerberjg.gdxstudio.entities;
 
-public abstract class EntitySystem {
-	
+import com.badlogic.gdx.utils.Disposable;
+
+public abstract class EntitySystem implements Disposable {
+	/** Best to call after every frame */
+	public abstract void cleanup();
 }
