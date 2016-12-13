876d47b0ac359bbd95f51b96cda5121fbb7731f3
Jakub Kerber <kerber.jg@gmail.com>
2016-12-13 21:33:14 +0100

    Updated the utility collection classes

--- src/com/kerberjg/gdxstudio/utils/collections/FastIntMap.java
+++ src/com/kerberjg/gdxstudio/utils/collections/FastIntMap.java
@@ -1,10 +1,15 @@
 package com.kerberjg.gdxstudio.utils.collections;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.utils.Bits;
 import com.badlogic.gdx.utils.reflect.ArrayReflection;
 
-/** A speed-optimized map which uses integers as keys. Always preserves the index and never moves the values around */
-public class FastIntMap<V> {
+/** A speed-optimized map which uses integers as keys. Always preserves the index and never moves the values around
+ * 
+ * @author kerberjg
+ */
+public class FastIntMap<V> implements Iterable<V> {
 	public V[] items;
 	public Bits bits;
 	
@@ -17,7 +22,7 @@ public class FastIntMap<V> {
 	
 	@SuppressWarnings("unchecked")
 	public FastIntMap(int capacity) {
-		items = (V[])new Object[capacity];
+		items = (V[]) ArrayReflection.newInstance(Object.class, capacity);
 		bits = new Bits(capacity);
 	}
 
@@ -33,10 +38,12 @@ public class FastIntMap<V> {
 	}
 
 	public boolean isEmpty() {
-		return bits.isEmpty();
+		return size == 0;
 	}
 
-	public int add(V e) {
+	/**	Puts the element at the first free position
+	 * @return the element ID */
+	public int put(V e) {
 		if(this.size == items.length) {
 			this.grow((int) (size * growthFactor));
 			bits.set(items.length - 1);
@@ -50,8 +57,34 @@ public class FastIntMap<V> {
 		return i;
 	}
 	
+	/** Puts the element at a specified position
+	 * @return previously stored object if any, otherwise null */
+	public V put(int i, V e) {
+		V prev;
+		
+		// Checks if growth is needed
+		if(this.size - 1 < i) {
+			grow(i + 1);
+			prev = null;
+		} else 
+			prev = get(i);
+		
+		// Increment size only if no previous element was there
+		if(bits.getAndSet(i)) ++size;
+		
+		items[i] = e;
+		return prev;
+	}
+	
+	/**
+	 * @param i the index of the element to return
+	 * @return the requested element if present, otherwise null */
 	public V get(int i) {
 		return items[i];
+	}
+	
+	public boolean containsKey(int key) {
+		return bits.get(key);
 	}
 
 	public V remove(int index) {
@@ -65,7 +98,7 @@ public class FastIntMap<V> {
 		int sizeBefore = this.size;
 		
 		for(V e : c)
-			this.add(e);
+			this.put(e);
 		
 		return (this.size - sizeBefore > 0);
 	}
@@ -74,6 +107,31 @@ public class FastIntMap<V> {
 		bits.clear();
 		for(int i = 0; i < items.length; i++)
 			items[i] = null;
+	}
+
+	@Override
+	public Iterator<V> iterator() {
+		return new FastIntMapIterator<V>();
+	}
+	
+	private class FastIntMapIterator<T> implements Iterator<V> {
+		private int i = 0;
+
+		@Override
+		public boolean hasNext() {
+			return bits.nextSetBit(i) != -1;
+		}
+
+		@Override
+		public V next() {
+			int next = bits.nextSetBit(i++);
+			
+			if(next >= 0)
+				return items[next];
+			else
+				return null;
+		}
+		
 	}
 
 }
