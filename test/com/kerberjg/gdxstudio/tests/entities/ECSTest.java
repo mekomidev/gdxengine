3360c720306177709f856fc1f36f9881ca5ff7d1
Jakub Kerber <kerber.jg@gmail.com>
2016-12-13 21:40:00 +0100

    Put down a base for ECS tests

---
+++ test/com/kerberjg/gdxstudio/tests/entities/ECSTest.java
@@ -0,0 +1,16 @@
+package com.kerberjg.gdxstudio.tests.entities;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.kerberjg.gdxstudio.entities.*;
+
+public class ECSTest {
+
+	@Test
+	public void test() {
+		EntityManager manager = new EntityManager();
+		
+		
+	}
+
+}
