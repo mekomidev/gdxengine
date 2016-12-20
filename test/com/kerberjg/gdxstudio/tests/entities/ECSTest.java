e46e52920a808882c53e567615faa72c348d266a
Jakub Kerber <kerber.jg@gmail.com>
2016-12-20 22:38:31 +0100

    Tests
    self-explanatory

--- test/com/kerberjg/gdxstudio/tests/entities/ECSTest.java
+++ test/com/kerberjg/gdxstudio/tests/entities/ECSTest.java
@@ -2,13 +2,17 @@ package com.kerberjg.gdxstudio.tests.entities;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+
 import com.kerberjg.gdxstudio.entities.*;
 
 public class ECSTest {
 
 	@Test
 	public void test() {
+		// TODO: learn to write tests
 		EntityManager manager = new EntityManager();
+		
+		manager.addSystem(new TestSystem());
 		
 		
 	}
---
+++ test/com/kerberjg/gdxstudio/tests/entities/TestSystem.java
@@ -0,0 +1,37 @@
+package com.kerberjg.gdxstudio.tests.entities;
+
+import com.kerberjg.gdxstudio.entities.EntitySystem;
+
+public class TestSystem extends EntitySystem {
+	int base;
+	long x;
+	boolean dirty;
+
+	@Override
+	public void init() {
+		base = (int) (Math.random() * 1000D);
+		dirty = false;
+	}
+	
+	@Override
+	public void update(float delta) {
+		x += delta * base;
+		
+		dirty = true;
+	}
+
+	@Override
+	public void render() {
+		System.out.print("Rendered frame");
+	}
+
+	@Override
+	public void cleanup() {
+		dirty = false;
+	}
+
+	@Override
+	public void dispose() {
+		base = 0;
+	}
+}
