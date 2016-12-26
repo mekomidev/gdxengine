26087bd235819d172c6ff36067532f7ed40062e2
Jakub Kerber <kerber.jg@gmail.com>
2016-12-26 23:21:33 +0100

    Written a skeleton for JsonStageBuilder

---
+++ src/com/kerberjg/gdxstudio/core/utils/builders/JsonStageBuilder.java
@@ -0,0 +1,25 @@
+package com.kerberjg.gdxstudio.core.utils.builders;
+
+import com.badlogic.gdx.utils.JsonValue;
+import com.kerberjg.gdxstudio.core.Stage;
+import com.kerberjg.gdxstudio.core.Stage.StageBuilder;
+
+public class JsonStageBuilder extends StageBuilder {
+
+	public JsonStageBuilder(JsonValue definition) {
+		// Get stage properties
+		
+		// Get entity systems
+		
+		// Get entities
+		
+	}
+	
+	@Override
+	public Stage build() {
+		Stage s = getStageInstance();
+		// TODO Auto-generated method stub
+		return s;
+	}
+
+}
