b1e2b101d769dc12099a22ee96164e968534fb80
Jakub Kerber <kerber.jg@gmail.com>
2017-01-23 23:23:04 +0100

    Documented the Entity class

--- src/com/kerberjg/gdxstudio/core/entities/Entity.java
+++ src/com/kerberjg/gdxstudio/core/entities/Entity.java
@@ -1,52 +1,60 @@
 package com.kerberjg.gdxstudio.core.entities;
 
-
+/**	An abstract Entity.
+ * All of the ECS functionalities are implemented, what's left to the user is just the general behavior
+ * 
+ * @author kerberjg*/
 public abstract class Entity implements PrimitiveEntity {
 	private final EntityManager manager;
 	public final int id;
 	public final String name;
 	
+	/** Constructs an Entity and registers it within an indicated EntityManager
+	 * 
+	 * @param manager the EntityManager to register the Entity in
+	 * @param name the name to register this Entity with; has to be unique */
+	// TODO: this API is a mess (or is it...?)
 	public Entity(EntityManager manager, String name) {
 		this.manager = manager;
 		this.name = name;
 		this.id = manager.addEntity(name, this);
 	}
 	
+	/** Triggers an event on this object
+	 * 
+	 * @param eventName self-explanatory
+	 * @param blob optional parameters to pass as message(s) for this event */
 	public abstract void triggerEvent(String eventName, Object... blob);
 	
 	/*
 	 * 	Component management
 	 */
 
-	/**
-	 * @param componentType the class of the Component to add to the Entity
-	 * @return the added Component instance */
+	/** @return the added Component instance
+	 * @param componentType the class of the Component to add to the Entity */
 	public <C extends Component> C addComponent(Class<C> componentType) {
 		C component = Components.getComponentInstance(componentType);
 		manager.addComponent(id, component);
 		return componentType.cast(component);
 	}
 	
-	/**
-	 * @param componentId the type ID of the Component to return
-	 * @return the requested Component */
+	/** @return the requested Component
+	 * @param componentId the type ID of the Component to return */
 	public <C extends Component> C getComponent(Class<C> componentType) {
 		int componentId = Components.getComponentId(componentType);
 		
 		return componentType.cast(manager.getComponent(id, componentId));
 	}
 	
-	/**
-	 * @param componentType the class of the Component to remove
-	 * @return whether the Component was removed*/
+	/** @return whether the Component was removed
+	 * @param componentType the class of the Component to remove */
 	public <C extends Component> boolean removeComponent(Class<C> componentType) {
 		int componentId = Components.getComponentId(componentType);
 		return manager.removeComponent(id, componentId);
 	}
 	
-	/**
-	 * @param componentId the type ID of the Component to remove
-	 * @return whether the Component was removed*/
+	/** @return whether the Component was removed
+	 * @param componentId the type ID of the Component to remove */
 	public <C extends Component> boolean removeComponent(int componentId) {
 		return manager.removeComponent(id, componentId);
 	}
