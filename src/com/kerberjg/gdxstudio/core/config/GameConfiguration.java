64fc319f3dba662f44216bc6091996f76d1417f7
Jakub Kerber <kerber.jg@gmail.com>
2017-02-13 16:53:07 +0100

    GameConfiguration classes for easier configuration organization

---
+++ src/com/kerberjg/gdxstudio/core/config/GameConfiguration.java
@@ -0,0 +1,35 @@
+package com.kerberjg.gdxstudio.core.config;
+
+public class GameConfiguration {
+	/** Initial FPS limit;
+	 * if <= 0, the framerate is not limited;
+	 * default: 60 */
+	public int fps;
+	/** VSync flag;
+	 * default: false */
+	public boolean vsync;
+	/** Scalling factor of the frame delta time
+	 * ignored when <= 0;
+	 * default: 1 */
+	public float deltaScale;
+	/** Name of the first stage to open;
+	 * the game will load the first stage from the list if this is null;
+	 * default: null */
+	public String firstStage;
+	
+	/** Sets default settings */
+	public GameConfiguration() {
+		fps = 60;
+		vsync = false;
+		deltaScale = 1f;
+		firstStage = null;
+	}
+	
+	/** Accepts customized settings */
+	public GameConfiguration(int fps, boolean vsync, float deltaScale, String stage) {
+		this.fps = fps;
+		this.vsync = vsync;
+		this.deltaScale = deltaScale;
+		this.firstStage = stage;
+	}
+}
