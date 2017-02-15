08b275702d349202169ea37d25abd0ad9716d719
Jakub Kerber <kerber.jg@gmail.com>
2017-02-15 19:23:23 +0100

    Fixed SpritePainterSystem

--- src/com/kerberjg/gdxstudio/core/graphics/sprites/SpritePainterSystem.java
+++ src/com/kerberjg/gdxstudio/core/graphics/sprites/SpritePainterSystem.java
@@ -11,9 +11,7 @@ import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.ReflectionPool;
 import com.kerberjg.gdxstudio.core.Game;
 import com.kerberjg.gdxstudio.core.ecs.Component;
-import com.kerberjg.gdxstudio.core.ecs.EntityManager;
 import com.kerberjg.gdxstudio.core.ecs.EntitySystem;
-import com.kerberjg.gdxstudio.core.utils.collections.FastIntMap;
 
 /** Sprite rendering system implemented using the Painter's Algorithm.
  * Sorts sprites by depth before rendering them. 
@@ -29,8 +27,6 @@ public class SpritePainterSystem extends EntitySystem {
 	/** Pool of Sprite vessels to use as copies */
 	private Pool<Sprite> vessels;
 	
-	private int spriteComponentType;
-	
 	public SpritePainterSystem() {
 		this(DEFAULT_MAX_SPRITES);
 	}
@@ -41,35 +37,50 @@ public class SpritePainterSystem extends EntitySystem {
 		vessels = new ReflectionPool<>(Sprite.class, maxSprites);
 	}
 	
+	private static int[] components;
+	
 	@Override
-	public void init() {
-		spriteComponentType = Component.map.getSubclassId(SpriteComponent.class);
+	protected int[] init() {
+		if(components == null) {
+			components = new int[1];
+			components[0] = Component.map.getSubclassId(SpriteComponent.class);
+		}
+		
+		return components;
 	}
 
+	ArrayList<SpriteComponent> sortedComponents = new ArrayList<>();
+	
 	@Override
-	public void update(float delta) {
-		EntityManager manager = Game.stage;
-		FastIntMap<SpriteComponent> components = manager.getComponentMap(spriteComponentType);
-		ArrayList<SpriteComponent> sortedComponents = new ArrayList<>(components.size());
-		
-		// Copies all items to the new array
-		for(SpriteComponent sc : components)
-			if(sc.visible && sc.sprite != null)
-				sortedComponents.add(sc);
-		
+	public void updateBegin(float delta) {
+		batch.setProjectionMatrix(Game.stage.camera.combined);
+	}
+	
+	@Override
+	public <C extends Component> void updateStep(C c) {
+		SpriteComponent sc = (SpriteComponent) c;
+		if(sc != null && sc.sprite != null && sc.sprite.getTexture() != null && sc.visible)
+			sortedComponents.add(sc);
+				
+	}
+	
+	@Override
+	public void updateEnd() {
 		// Sorts the sprites
 		Collections.sort(sortedComponents, new DepthComparator());
 		
 		// Adds the sprites to the rendering queue
-		for(SpriteComponent sc : components) {
+		for(SpriteComponent sc : sortedComponents) {
 			Sprite s = vessels.obtain();
 			s.set(sc.sprite);
+			sprites.add(s);
 		}
+		
+		sortedComponents.clear();
 	}
 
 	@Override
 	public void dispose() {
-		cleanup();
 		vessels.clear();
 	}
 
@@ -84,10 +95,6 @@ public class SpritePainterSystem extends EntitySystem {
 		}
 		
 		batch.end();
-	}
-
-	@Override
-	public void cleanup() {
 		sprites.clear();
 	}
 	
@@ -97,13 +104,16 @@ public class SpritePainterSystem extends EntitySystem {
 	private static class DepthComparator implements Comparator<SpriteComponent> {
 		@Override
 		public int compare(SpriteComponent a, SpriteComponent b) {
-			// Checks for null cases, putting them at the end of the list
+			//INFO: shouldn't be null, due updateStep() already checking for nullability 
+			// Checks for null cases, putting null items at the end of the list
+			/*
 			if(a == null)
 				return 1;
 			else if (b == null)
 				return -1;
+			
 			// Does actual sorting
-			else if(a.depth == b.depth)
+			else*/ if(a.depth == b.depth)
 				return 0;
 			else if(a.depth > b.depth)
 				return 1;
