aee1a4a5baae7306f61b4822332e2514fcdb5f7b
Jakub Kerber <kerber.jg@gmail.com>
2017-05-07 18:36:15 +0200

    Added a simple units' class

---
+++ core/src/com/mekomidev/gdxengine/core/utils/Units.java
@@ -0,0 +1,10 @@
+package com.mekomidev.gdxengine.core.utils;
+
+public abstract class Units {
+	//TODO: kilobytes... or KIBIBYTES?
+	public static final long BYTE = 1;
+	public static final long KILOBYTE = 1024;
+	public static final long MEGABYTE = 1024 * KILOBYTE;
+	public static final long GIGABYTE = 1024 * MEGABYTE;
+	public static final long TERABYTE = 1024 * MEGABYTE;
+}
